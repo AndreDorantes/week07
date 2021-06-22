b5fa2a987c730fd6692a53edc8f0c00efbe7d5f1
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-21 23:55:26 -0500

    Add files via upload

---
+++ ADA WEEK07/chapter7ex07(OWN)/main.c
@@ -0,0 +1,22 @@
+/* This program calculates the area of a triangle with the heron formula */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+	float a, b, c, s, A;
+	printf ("This program calculates the area of a triangle with the heron formula \n");
+	printf ("Introduce the first side of the triangle: ");
+	scanf ("%f", &a);
+	printf ("Introduce the second side of the triangle: ");
+	scanf ("%f", &b);
+	printf ("Introduce the third side of the triangle: ");
+	scanf ("%f", &c);
+	s= 0.5*(a+b+c);
+	A= pow(s*(s-a)*(s-b)*(s-c),0.5);
+	printf ("The area of the triangle is: %f",A);
+	
+	getch();
+	return 0;
+}
