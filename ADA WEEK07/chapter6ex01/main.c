7420d01a16af13383c49e7d298687940a225dc24
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:06:11 -0500

    Update main.c

--- ADA WEEK07/chapter6ex01/main.c
+++ ADA WEEK07/chapter6ex01/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	float a, b, c, d, e;
 	printf("Insert the coordinates in x of the first point: ");
-	scanf("%f",&a);
+	fscanf(stdin,"%f",&a);
 	printf("\n Insert the coordinates in y of the first point: ");
-	scanf("%f",&b);	
+	fscanf(stdin, "%f",&b);		
 	printf("\n Insert the coordinates in x of the second point: ");
-	scanf("%f",&c);
+	fscanf(stdin, "%f",&c);
 	printf("\n Insert the coordinates in y of the second point: ");
-	scanf("%f",&d);
+	fscanf(stdin, "%f",&d);
 	e= (c-a)*(c-a)+(d-b)*(d-b);
 	printf ("The square distance of the points is: %f", e);
 	getch();
