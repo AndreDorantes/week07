40943e537767c74b83dfccac265c40ade0fed961
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:26:30 -0500

    Update main.c

--- ADA WEEK07/chapter6ex06/main.c
+++ ADA WEEK07/chapter6ex06/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	float a, b, p, p2, o;
 	printf ("Insert the number of hours worked: ");
-	scanf ("%f", &a);
+	fscanf (stdin, "%f", &a);
 	printf ("Insert the the hourly wage: ");
-	scanf ("%f", &b);
+	fscanf (stdin, "%f", &b);
 	p= a*b;
 	o= a-40;
 	p2= (40*b)+o*(1.5*b);
