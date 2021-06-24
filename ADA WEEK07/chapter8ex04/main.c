7bc6ddc257c309b83d10412e507458c324ece28c
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:43:50 -0500

    Update main.c

--- ADA WEEK07/chapter8ex04/main.c
+++ ADA WEEK07/chapter8ex04/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	int n, i;
     printf("Wich multiplication table do you want?: ");
-    scanf("%d", &n);
+    fscanf(stdin, "%d", &n);
     for (i = 1; i <= 10; ++i) {
         printf("%d * %d = %d \n", n, i, n*i);
     }
