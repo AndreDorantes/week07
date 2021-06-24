396c3efd6405a9fb688197c6f51c26543b5c6da7
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:41:58 -0500

    Update main.c

--- ADA WEEK07/chapter8ex02/main.c
+++ ADA WEEK07/chapter8ex02/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   int n, count= 1;   
   float x, sum= 0;   
   printf("How many resistors do you have? \n");  
-  scanf ("%d",&n);   
+  fscanf (stdin, "%d",&n);   
   while (count <= n)   
      {   
       printf ("Enter the %d resistence \n",count);   
-      scanf("%f", &x);   
+      fscanf(stdin, "%f", &x);   
       sum += (1/x);   
       ++count;   
      }   
