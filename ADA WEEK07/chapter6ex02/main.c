9d3b04c3745e3e34f0e8ff4149b302b367858161
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:20:15 -0500

    Update main.c

--- ADA WEEK07/chapter6ex02/main.c
+++ ADA WEEK07/chapter6ex02/main.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 
 int main(int argc, char *argv[]) {
-	else 
-				          						if (a>=94 && a<98)
-				          				 		printf ("Your score is A");else 
-				          						if (a>=94 && a<98)
-				          				 		printf ("Your score is A");else 
-				          						if (a>=94 && a<98)
-				          				 		printf ("Your score is A");else 
-				          						if (a>=94 && a<98)
-				          				 		printf ("Your score is A");else 
-				          						if (a>=94 && a<98)
-				          				 		printf ("Your score is A");else 
-				          						if (a>=94 && a<98)
-				          				 		printf ("Your score is A");else 
-				          						if (a>=94 && a<98)
-				          				 		printf ("Your score is A");else 
-				          						if (a>=94 && a<98)
-				          				 		printf ("Your score is A");else 
-				          						if (a>=94 && a<98)
-				          				 		printf ("Your score is A");else 
-				          						if (a>=94 && a<98)
-				          				 		printf ("Your score is A");
+	float a;
+	printf ("Insert the numeric grade:");
+	fscanf (stdin, "%f", &a );
+	if (a<61) 
+		printf ("Your score is F");
+		else 
+		    if (a>=61 && a<71)
+		     printf ("Your score is D");
+		     else 
+		     	 	if (a>=71 && a<81)
+		    	     printf ("Your score is C");
+		    	    	else 
+				            if (a>=81 && a<91)
+				               	printf ("Your score is B");
+				               	else 
+				          					if (a>=91 && a<=100)
+				          				 	printf ("Your score is A");
+				          				 	else
+				          				 	printf ("The range in from 0-100"); 
+				          						
 	
 	getch();
 	
