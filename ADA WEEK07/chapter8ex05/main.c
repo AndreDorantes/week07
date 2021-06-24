8b34698d1532ed7412d54afbbc8ec1eb2f180afa
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:44:14 -0500

    Update main.c

--- ADA WEEK07/chapter8ex05/main.c
+++ ADA WEEK07/chapter8ex05/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	char a;
 	printf ("Insert the letter:" );
-	scanf ("%c", &a);
+	fscanf (stdin, "%c", &a);
 	if (a== 'a' || a== 'e' || a== 'i' || a== 'o' || a== 'u')
 			printf ("Vowel");
 	else 
