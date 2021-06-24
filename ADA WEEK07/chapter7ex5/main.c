72dd801dc961f553bf7a9201f2d9f2a6e84d17bc
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:34:35 -0500

    Update main.c

--- ADA WEEK07/chapter7ex5/main.c
+++ ADA WEEK07/chapter7ex5/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	/* Variables you will use in the program*/
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
     do
     {
         printf("Number: \n");
-        scanf(" %d",&num);
+        fscanf(stdin, "%d",&num);
        prim=0;
 div=2;
             while(div<num  && prim!=1)
