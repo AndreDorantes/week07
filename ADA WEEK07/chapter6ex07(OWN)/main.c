f80c23498b0b048475710ed9bf41cebab5538788
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:27:20 -0500

    Update main.c

--- ADA WEEK07/chapter6ex07(OWN)/main.c
+++ ADA WEEK07/chapter6ex07(OWN)/main.c
@@ -2,14 +2,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	float a, b, c, h;
 	printf ("Instert side a:");
-	scanf ("%f", &a);
+	fscanf (stdin, "%f", &a);
 	printf ("Instert side b:");
-	scanf ("%f", &b);
+	fscanf (stdin, "%f", &b);
 	c= (a*a+b*b);
 	h= pow(c, 0.5);
 	printf ("The hypotenuse of the right triangle is: %f", h);
