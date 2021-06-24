52e7834ee4e2e14045b9276d400a35fb267bda1f
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:22:45 -0500

    Update main.c

--- ADA WEEK07/chapter6ex03/main.c
+++ ADA WEEK07/chapter6ex03/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 
 int main(int argc, char *argv[]) {
 	float a;
 	printf ("Insert the numeric grade:");
-	scanf ("%f", &a );
+	fscanf (stdin, "%f", &a );
 	if (a<61) 
 		printf ("Your score is F");
 		else 
