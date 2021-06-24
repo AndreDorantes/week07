d2b364963be299da1de4ad6948b7b9be96af1143
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:32:30 -0500

    Update main.c

--- ADA WEEK07/chapter7ex1/main.c
+++ ADA WEEK07/chapter7ex1/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	int a;
@@ -11,28 +11,28 @@ int main(int argc, char *argv[]) {
 	printf ("[3] For Volume \n");
 	printf ("[4] For Mass \n");
 	printf ("Please enter your choice: \n");
-	scanf ("%i", &a);
+	fscanf (stdin, "%i", &a);
 	if (a==1){
 			printf ("Please introduce the miles you want to convert to kilometers: \n");
-			scanf("%f", &b);
+			fscanf(stdin, "%f", &b);
 			Km= b*1.60934;
 			printf ("The convertion to kilometers is:%f", Km);}
 				else 
 				if(a==2){
 						printf ("Please introduce the temperature in fahrenheit you want to convert to celcius: \n");
-						scanf("%f", &c);
+						fscanf(stdin, "%f", &c);
 						C= (c-32)*0.5555;
 						printf ("The convertion to celcius is:%f", C);}
 					else 
 					if(a==3){
 							printf ("Please introduce the gallons you want to convert to liters: \n");
-							scanf("%f", &d);
+							fscanf(stdin, "%f", &d);
 							L= d*3.785;
 							printf ("The convertion to liters is:%f", L);}
 						else 
 						if (a==4){
 									printf ("Please introduce the pounds you want to convert to kilograms: \n");
-									scanf("%f", &e);
+									fscanf(stdin, "%f", &e);
 									Kg= e/2.205;
 									printf ("The convertion to kilograms is:%f", Kg);}
 							else 
