9a9aa7756e923cb24cf78802925e4faf4e88fdc9
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:25:43 -0500

    Update main.c

--- ADA WEEK07/chapter6ex05/main.c
+++ ADA WEEK07/chapter6ex05/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	int year;
 	printf("Insert the year: ");
-	scanf("%d",&year);
+	fscanf(stdin, "%d",&year);
 	if(year % 4 == 0 && year% 100 != 0 || year % 400 == 0 )
         printf( "\n   Is a leap year" );
     else
