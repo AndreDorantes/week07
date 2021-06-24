2f82a9116b26ee6ef6978e50cc7cd3e331bc6bc6
AndreDorantes <83776597+AndreDorantes@users.noreply.github.com>
2021-06-24 18:43:28 -0500

    Update main.c

--- ADA WEEK07/chapter8ex03/main.c
+++ ADA WEEK07/chapter8ex03/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   int n, count= 1;   
   float x, avrg, sum= 0;   
   printf("How many numbers do you have? \n");  
-  scanf ("%d",&n);   
+  fscanf (stdin, "%d",&n);   
   while (count <= n)   
      {   
       printf ("Enter the %d number \n",count);   
-      scanf("%f", &x);   
+      fscanf(stdin, "%f", &x);   
       sum += x;   
       ++count;   
      }   
